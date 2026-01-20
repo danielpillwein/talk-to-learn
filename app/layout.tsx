5bef62cba3aac86ae85d1b33e8c55db07924c86b
Daniel Pillwein <105200276+danielpillwein@users.noreply.github.com>
2026-01-20 12:38:13 +0100

    Refactor RootLayout to use Script component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,25 +6,30 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Talk to Learn",
-    description: "Practice answering questions with voice",
+  title: "Talk to Learn",
+  description: "Practice answering questions with voice",
 };
 
 export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="de">
-            <head>
-                <!--Stats-->
-                <script defer src="https://cloud.umami.is/script.js" data-website-id="45361192-66d0-46ad-ba1e-c55cfdaa9d81"></script>
-                <Script
-                    id="mathjax-config"
-                    strategy="beforeInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="de">
+      <head>
+        {/* Stats */}
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="45361192-66d0-46ad-ba1e-c55cfdaa9d81"
+          strategy="afterInteractive"
+        />
+
+        <Script
+          id="mathjax-config"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
               window.MathJax = {
                 tex: {
                   inlineMath: [['\\\\(', '\\\\)']],
@@ -36,15 +41,17 @@ export default function RootLayout({
                 }
               };
             `,
-                    }}
-                />
-                <Script
-                    id="mathjax-script"
-                    src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
-                    strategy="beforeInteractive"
-                />
-            </head>
-            <body className={inter.className}>{children}</body>
-        </html>
-    );
+          }}
+        />
+
+        <Script
+          id="mathjax-script"
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+          strategy="beforeInteractive"
+        />
+      </head>
+
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
